546c56ff440206638191628007f2b1d00a3154e3
Nishi-majawdiya <nishi.majawdiya@novadule.com>
2026-03-11 12:08:54 +0530

    Added CI/CD test message

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,16 +18,12 @@ namespace MicroservicesSolution.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+
+        [HttpGet]
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("CI/CD Test Version 1");
         }
+
     }
 }
